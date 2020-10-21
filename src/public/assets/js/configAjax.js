e6c5aa9e680dc434928b49d4965814c10e1e485b
DanielAviiles <srhackme@gmail.com>
2020-10-21 14:11:58 -0500

    Nueva vista de tabla para ordenadores

--- src/public/assets/js/configAjax.js
+++ src/public/assets/js/configAjax.js
@@ -1,35 +1,77 @@
+/* Formatting function for row details - modify as you need */
+function format ( d ) {
+  // `d` is the original data object for the row
+  return `<div class="col-lg-12 table-responsive">
+            <table>
+              <tr>
+                <td><b>Fecha Adquisicion: </b></td>
+                <td>${d.fecha_adquision}</td>
+              </tr>
+              <tr>
+                <td><b>Empresa / Proveedor: </b></td>
+                <td>${d.nombre_empresa}</td>
+              </tr>
+              <tr>
+                <td><b>NIT Empresa: </b></td>
+                <td>${d.nit}</td>
+              </tr>
+              <tr>
+                <td><b>Obejto del contrado: </b></td>
+                <td>${d.objeto_contrato}</td>
+              </tr>
+            </table>
+          </div>`;
+}
+
 $(document).ready(function () {
-  const dataEquipo = $('#tableOrdenadores').DataTable( {
+  let ordenadores = $('#ordenadorTable').DataTable({
+    "responsive": true, "lengthChange": true, "autoWidth": false,
+    lengthMenu: [[5, 10, 25, 50, -1], [5, 10, 25, 50, "All"]],
+    "language": {
+      "zeroRecords": "No hay datos disponibles en la tabla",
+      "info": "Mostrando _START_ a _END_ de _TOTAL_ entradas",
+      "infoEmpty": "No hay registros disponibles",
+      "lengthMenu": "Mostrar _MENU_ entradas",
+      "search": "Buscar: ",
+      "paginate": {
+        "next": "Siguiente",
+        "previous": "Anterior"
+      }
+    },
     "ajax": "/ordenadores/info-ordenadores",
     "columns": [
       {
         "className":      'details-control',
         "orderable":      false,
         "data":           null,
-        "defaultContent": ''
+        "defaultContent": '<i class="fas fa-plus-circle text-center" style="cursor: pointer;"></i>'
       },
-      { "data": "name" },
-      { "data": "position" },
-      { "data": "office" },
-      { "data": "salary" }
+      { "data": "codigo_equipo" },
+      { "data": "nombre_completo" },
+      { "data": "tipo_equipo" },
+      { "data": "estado_nombre" }
     ],
-    "order": [[1, 'asc']]
-} );
+    select: {
+      style:    'os',
+      selector: 'td:first-child'
+    },
+    "order": [[1, 'desc']]
+  });
 
   // Add event listener for opening and closing details
-  $('#example tbody').on('click', 'td.details-control', function () {
-      var tr = $(this).closest('tr');
-      var row = table.row( tr );
+  $('#ordenadorTable tbody').on('click', 'td.details-control', function () {
+    var tr = $(this).closest('tr');
+    var row = ordenadores.row(tr);
 
-      if ( row.child.isShown() ) {
-          // This row is already open - close it
-          row.child.hide();
-          tr.removeClass('shown');
-      }
-      else {
-          // Open this row
-          row.child( format(row.data()) ).show();
-          tr.addClass('shown');
-      }
-  } );
+    $(this)
+    .find('[data-fa-i2svg]')
+    .toggleClass('fa-minus-circle')
+    .toggleClass('fa-plus-circle');
+
+    if ( row.child.isShown() ) {
+      row.child.hide();
+    } else {
+      row.child( format(row.data()) ).show();
+    }
+  });
 });
--- src/routes/listaOrdenadores.js
+++ src/routes/listaOrdenadores.js
@@ -7,13 +7,19 @@ router.get('/lista-ordenadores', (req, res) => {
 });
 
 router.get('/info-ordenadores', async(req, res) => {
-  const infoOrdenadoresFull = await pool.query(`SELECT 
-                                                  equipo.codigo,
-                                                  equipo.fecha_adquision
-                                                FROM
-                                                  equipo`);
+  const infoEquipos = await pool.query(`SELECT 
+                                          equipo.codigo_equipo, tipo_equipo.nombre tipo_equipo, equipo.fecha_adquision,
+                                          usuario.nombre_completo, equipo.objeto_contrato,
+                                          empresa_proveedor.nombre nombre_empresa, empresa_proveedor.nit,
+                                          estado_pc.nombre estado_nombre
+                                        FROM
+                                          equipo, tipo_equipo, usuario, empresa_proveedor, estado_pc
+                                        WHERE
+                                          equipo.tipo_equipo_id = tipo_equipo.id AND equipo.usuario_id = usuario.id AND
+                                          equipo.empresa_proveedor_id = empresa_proveedor.id AND
+                                          equipo.estado_pc_id = estado_pc.id`);
 
-  res.json({ infoOrdenadoresFull });
+  res.json({ data: infoEquipos });
 });
 
 module.exports = router;
